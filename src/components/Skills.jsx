03e847ea13de6c087042b7271f5071148cdcd557
Ryusei Harada <is0323hi@ed.ritsumei.ac.jp>
2017-12-17 14:49:57 +0900

    fix some

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,23 +4,19 @@ const Skills = () => {
   return (
     <div id={ 'Skills' } className={ 'Skills' }>
       <h2>Skills</h2>
-      <p>
-        <ul>
-          <li>{ 'JavaScript' }</li>
-          <li>{ 'Clang' }</li>
-          <li>{ 'PHP' }</li>
-        </ul>
-      </p>
+      <ul>
+        <li>{ 'JavaScript' }</li>
+        <li>{ 'Clang' }</li>
+        <li>{ 'PHP' }</li>
+      </ul>
 
       <h2>Studing</h2>
-      <p>
-        <ul>
-          <li>{ 'Python' }</li>
-          <li>{ 'Golang' }</li>
-          <li>{ 'React' }</li>
-          <li>{ 'Network Security' }</li>
-        </ul>
-      </p>
+      <ul>
+        <li>{ 'Python' }</li>
+        <li>{ 'Golang' }</li>
+        <li>{ 'React' }</li>
+        <li>{ 'Network Security' }</li>
+      </ul>
     </div>
   )
 }
