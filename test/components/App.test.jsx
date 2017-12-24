b9aa7f8ed8a2414fcbaeab3c6b09a2cad64ce803
Ryusei Harada <is0323hi@ed.ritsumei.ac.jp>
2017-12-25 00:53:19 +0900

    fix test

--- test/components/App.test.jsx
+++ test/components/App.test.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { expect } from 'chai'
 import { shallow } from 'enzyme'
-import Home from '../../src/components/Home'
+import About from '../../src/components/Header'
 
 
-describe('<Home />', () => {
+describe('<Header />', () => {
   it('should render single <h1>', () => {
-    const wrapper = shallow(<Home />)
-    expect(wrapper.find('#Home')).to.have.length(1)
+    const wrapper = shallow(<About />)
+    expect(wrapper.find('#Header')).to.have.length(1)
   })
 })
