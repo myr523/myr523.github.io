fc8447f86c3d1315d1b964c05d451372d5040f20
Ryusei HARADA <is0323hi@ed.ritsumei.ac.jp>
2018-05-17 23:00:53 +0900

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,11 +4,6 @@ import React from 'react'
 const Header = () => {
   return(
     <div id={ 'Header' }>
-      <img
-        className={ 'image-header' }
-        src={ 'https://raw.githubusercontent.com/myr523/myr523.github.io/develop/src/styles/assets/s512_xmas_santa_cap1_8.png' }
-        height="100"
-      />
       <header className={ 'Header' }>
         <h1 className={ 'name' }>Ryusei Harada</h1>
       </header>
